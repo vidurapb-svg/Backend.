bb07350e723f02b213f648e71e8ce755e1f0f64f
vidurapb-svg <vidurapb@gmail.com>
2026-04-24 08:44:05 +0530

    Check MONGO_URI and enhance MongoDB connection error logs
    Added environment variable check for MONGO_URI and improved error logging.

--- backend/server.js
+++ backend/server.js
@@ -5,23 +5,36 @@ const cors = require("cors");
 const itemRoutes = require("./routes/itemRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// ✅ check env first
+if (!process.env.MONGO_URI) {
+  console.log("❌ MONGO_URI is missing in environment variables");
+}
+
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
   })
   .catch((err) => {
-    console.log("❌ MongoDB ERROR_ failed to connect:");
-    console.log(err);
+    console.log("❌ MongoDB ERROR - failed to connect:");
+    console.log(err.message);
   });
 
 // Routes
 app.use("/", itemRoutes);
 
+// Health check route (VERY IMPORTANT for Railway)
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 // Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
